4690393f18cc27a0314cc5d0bed116e0e78ac3c9
middlemiddlemiddle <middlemiddlemiddle@proton.me>
2024-11-02 10:58:28 +1000

    Modify for trumppets: keep as png and modify rarities
    rarities and conditionals applied in nft.py

--- config.py
+++ config.py
@@ -20,68 +20,62 @@
 
 # Be sure to check out the tutorial in the README for more details.                
 
+# ALL RARITY WEIGHTS MUST ADD UP TO 1_000
+# In ruby
+# x = [1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5]
+# x_sum = x.sum
+# x.map! do |y|
+#   1000 / x_sum * y
+# end
+# x.sum
 CONFIG = [
     {
         'id': 1,
         'name': 'background',
-        'directory': 'Background',
+        'directory': '00_Background',
         'required': True,
-        'rarity_weights': [10, 10, 10, 10, 20, 20, 20, 20, 30, 30, 30, 30, 30, 40, 40, 40, 40, 40],
+        'rarity_weights': [22, 22, 34, 34, 48, 48, 48, 48, 48, 48, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60],
     },
     {
         'id': 2,
-        'name': 'who_dis?',
-        'directory': 'Who Dis',
+        'name': 'body',
+        'directory': '01_Body',
         'required': True,
-        'rarity_weights': None,
+        'rarity_weights': [8, 8, 16, 16, 16, 16, 25, 25, 25, 25, 25, 25, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 50, 50, 50, 50, 50, 50, 50, 50, 50],
     },
     {
         'id': 3,
-        'name': 'body',
-        'directory': 'Body',
+        'name': 'neck',
+        'directory': '02_Neck',
         'required': True,
-        'rarity_weights': [10, 10, 10, 20, 20, 20, 20, 20, 20, 20, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40],
+        'rarity_weights': [47, 69, 92, 92, 700],
     },
     {
         'id': 4,
         'name': 'face',
-        'directory': 'Face',
-        'required': False,
-        'rarity_weights': [210, 10, 40, 40],
+        'directory': '03_Face',
+        'required': True,
+        'rarity_weights': [142, 213, 284, 401],
     },
     {
         'id': 5,
-        'name': 'eyes',
-        'directory': 'Eyes',
+        'name': 'head',
+        'directory': '04_Head',
         'required': True,
-        'rarity_weights': [10, 20, 30, 30, 40, 40, 40],
+        'rarity_weights': [8, 12, 12, 15, 15, 15, 15, 15, 15, 23, 23, 23, 23, 23, 23, 23, 31, 31, 31, 31, 31, 31, 31, 50, 50, 50, 50, 300],
     },
     {
         'id': 6,
-        'name': 'head',
-        'directory': 'Head',
-        'required': False,
-        'rarity_weights': [330, 10, 10, 20, 20, 20, 20, 30, 30, 30, 30, 30, 30, 30, 30, 30, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40],
+        'name': 'glasses',
+        'directory': '05_Glasses',
+        'required': True,
+        'rarity_weights': [10, 40, 70, 90, 90, 700],
     },
     {
         'id': 7,
-        'name': 'face_mask',
-        'directory': 'Face Mask',
-        'required': False,
-        'rarity_weights': [230, 10, 20, 30, 40],
-    },
-    {
-        'id': 8,
-        'name': 'glasses',
-        'directory': 'Glasses',
-        'required': False,
-        'rarity_weights': [250, 10, 20, 20, 30, 30],
-    },
-    {
-        'id': 9,
-        'name': 'jabbed?',
-        'directory': 'Jabbed',
-        'required': False,
-        'rarity_weights': [40, 20],
+        'name': 'hand',
+        'directory': '06_Hand',
+        'required': True,
+        'rarity_weights': [4, 14, 14, 14, 14, 14, 21, 21, 21, 21, 21, 21, 34, 34, 34, 34, 34, 34, 34, 34, 34, 42, 42, 400],
     },
 ]
